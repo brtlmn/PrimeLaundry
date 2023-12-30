72823e7e217c2df59f7fe7899c062aa35d9d58b5
Edham Rabuan <ahmadaham99@gmail.com>
2023-12-31 01:08:15 +0800

    refactor admin registration

--- app/src/main/java/com/example/primelaundryfyp/Admin/registerAdmin.java
+++ app/src/main/java/com/example/primelaundryfyp/Admin/registerAdmin.java
@@ -4,50 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.primelaundryfyp.Customer.registerCustomer;
+import com.example.primelaundryfyp.FirebaseService;
 import com.example.primelaundryfyp.MainActivity;
+import com.example.primelaundryfyp.Model.User;
 import com.example.primelaundryfyp.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class registerAdmin extends AppCompatActivity {
 
     private EditText nameAdminReg, emailAdminReg, passwordAdminReg, phoneNumAdminReg;
     private Button signupAdminReg;
-    private FirebaseAuth firebaseAuth;
-    private FirebaseFirestore firebasefirestore;
+    private FirebaseService firebaseService;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_admin);
 
-
+        firebaseService = new FirebaseService();
         nameAdminReg = findViewById(R.id.nameAdminReg);
         emailAdminReg = findViewById(R.id.emailAdminReg);
         passwordAdminReg = findViewById(R.id.passwordAdminReg);
         phoneNumAdminReg = findViewById(R.id.phoneNumAdminReg);
         signupAdminReg = findViewById(R.id.signupAdminReg);
-        firebaseAuth = FirebaseAuth.getInstance();
         FirebaseApp.initializeApp(this);
-        firebasefirestore = firebasefirestore.getInstance();
 
         signupAdminReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,25 +43,24 @@ public class registerAdmin extends AppCompatActivity {
                 final String name = nameAdminReg.getText().toString();
                 final String phoneNumber = phoneNumAdminReg.getText().toString();
 
-                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                User userModel = new User();
+                userModel.setName(name);
+                userModel.setEmail(email);
+                userModel.setPassword(password);
+                userModel.setAddress(null);
+                userModel.setPhone_number(phoneNumber);
+                userModel.setUser_type(userModel.TYPE_ADMIN);
+
+                firebaseService.addUser(userModel, new FirebaseService.FirebaseListener() {
                     @Override
-                    public void onSuccess(AuthResult authResult) {
-                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                    public void onSuccess() {
                         Toast.makeText(registerAdmin.this, "Account Registered", Toast.LENGTH_SHORT).show();
-                        DocumentReference df = firebasefirestore.collection("Users").document(user.getUid());
-                        Map<String, Object> userinfo = new HashMap<>();
-                        userinfo.put("Name", nameAdminReg.getText().toString());
-                        userinfo.put("PhoneNumber", phoneNumAdminReg.getText().toString());
-                        userinfo.put("User Type", "Admin");
-                        df.set(userinfo);
-
-
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         finish();
                     }
-                }).addOnFailureListener(new OnFailureListener() {
+
                     @Override
-                    public void onFailure(@NonNull Exception e) {
+                    public void onFail(Exception e) {
                         Toast.makeText(registerAdmin.this, "Error!"+e, Toast.LENGTH_LONG).show();
                     }
                 });
