38a1bb616cb812f3e9ba57d12bc4e20c65f227d4
Edham Rabuan <ahmadaham99@gmail.com>
2023-12-31 00:55:21 +0800

    refactor register shop

--- app/src/main/java/com/example/primelaundryfyp/Shop/registerShop2.java
+++ app/src/main/java/com/example/primelaundryfyp/Shop/registerShop2.java
@@ -1,6 +1,5 @@
 package com.example.primelaundryfyp.Shop;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,27 +9,20 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.primelaundryfyp.FirebaseService;
 import com.example.primelaundryfyp.MainActivity;
+import com.example.primelaundryfyp.Model.Shop;
+import com.example.primelaundryfyp.Model.User;
 import com.example.primelaundryfyp.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class registerShop2 extends AppCompatActivity {
 
     private EditText addressShopReg, icNum;
     private Button signupShopReg;
-    private FirebaseAuth firebaseAuth;
-    private FirebaseFirestore firebasefirestore;
+    private FirebaseService firebaseService;
     private String name, email, password, phoneNumber, ssmNumber, address, icNumber;
 
     @Override
@@ -38,12 +30,11 @@ public class registerShop2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_laundry_shop2);
 
+        firebaseService = new FirebaseService();
         addressShopReg = findViewById(R.id.addressShopReg);
         icNum = findViewById(R.id.icNum);
         signupShopReg = findViewById(R.id.signupShopReg);
-        firebaseAuth = FirebaseAuth.getInstance();
         FirebaseApp.initializeApp(this);
-        firebasefirestore = FirebaseFirestore.getInstance();
         Intent intent = getIntent();
         ArrayList<String> shopReg = intent.getStringArrayListExtra("shopReg");
 
@@ -63,33 +54,32 @@ public class registerShop2 extends AppCompatActivity {
                 phoneNumber = shopReg.get(4);
                 address = shopReg.get(5);
 
+                User userModel = new User();
+                userModel.setName(name);
+                userModel.setEmail(email);
+                userModel.setPassword(password);
+                userModel.setAddress(address);
+                userModel.setPhone_number(phoneNumber);
+                userModel.setUser_type(userModel.TYPE_SHOP);
 
-                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                Shop shopModel = new Shop();
+                shopModel.setIc_num(icNumber);
+                shopModel.setSsm_number(ssmNumber);
+
+                firebaseService.addUserWithShop(userModel, shopModel, new FirebaseService.FirebaseListener() {
                     @Override
-                    public void onSuccess(AuthResult authResult) {
-                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                    public void onSuccess() {
                         Toast.makeText(registerShop2.this, "Account Registered", Toast.LENGTH_SHORT).show();
-                        DocumentReference df = firebasefirestore.collection("Users").document(user.getUid());
-                        Map<String, Object> userinfo = new HashMap<>();
-                        userinfo.put("Name", name);
-                        userinfo.put("SSMNumber", ssmNumber);
-                        userinfo.put("PhoneNumber", phoneNumber);
-                        userinfo.put("Address", addressShopReg.getText().toString());
-                        userinfo.put("ICNum", icNum.getText().toString());
-                        userinfo.put("User Type", "Shop");
-                        df.set(userinfo);
-
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         finish();
-                        //Toast.makeText(registerDriver2.this, "Error!"+e, Toast.LENGTH_LONG).show();
-
-                    }  }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(registerShop2.this, "Error!"+e, Toast.LENGTH_LONG).show();
                     }
 
+                    @Override
+                    public void onFail(Exception e) {
+                        Toast.makeText(registerShop2.this, "Error!"+e, Toast.LENGTH_LONG).show();
+                    }
                 });
+
             }
         });
     }
